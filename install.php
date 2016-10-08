0a06a1a32e3fe37f5f386cb061e8d8db65b2d7d8
Jesús Hernández <jeda@theworldev.net>
2016-10-08 17:45:04 -0400

    Unencrypted passwords x3 >:D

--- install.php
+++ install.php
@@ -155,11 +155,8 @@ $logSqlErrors = 0;
 	Upgrade();
 	Import(__DIR__.'/db/install.sql');
 	
-	$pss = Shake(16);
-	$sha = hash('sha256', $boardpassword.$salt.$pss, FALSE);
-	
-	Query("insert into {users} (id, name, password, pss, primarygroup, regdate, lastactivity, lastip, email, sex, theme) values ({0}, {1}, {2}, {3}, {4}, {5}, {5}, {6}, {7}, {8}, {9})", 
-		1, $boardusername, $sha, $pss, 4, time(), $_SERVER['REMOTE_ADDR'], '', 2, 'blargboard');
+	Query("insert into {users} (id, name, password, primarygroup, regdate, lastactivity, lastip, email, sex, theme) values ({0}, {1}, {2}, {3}, {4}, {5}, {5}, {6}, {7}, {8})", 
+		1, $boardusername, $boardpassword, 4, time(), $_SERVER['REMOTE_ADDR'], '', 2, 'blargboard');
 		
 ?>
 	<h3>Your new Blargboard board has been successfully installed!</h3>
